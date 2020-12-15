5f3798ff8a548d413679d08e3380547e47bd088b
Fyrklod <fyrklod@gmail.com>
2020-12-16 01:04:03 +0300

    Изменены скрипты миграции (дополнены и отформатированы посты)

--- src/main/resources/db/migration/V3__Fill_TableOfUser.sql
+++ src/main/resources/db/migration/V3__Fill_TableOfUser.sql
@@ -1,3 +1,7 @@
 INSERT INTO users (is_moderator, reg_time, name, email, password)
-     VALUES (true, '2020-04-05', 'TestModerator', 'testmail@mail.ru', '$2y$12$6h7kGRjjIfzwZo4yBCZEJeeJSzf61fRhBai9Jbdzm1OSTP7Kpqt3C'),
-     (false, '2020-05-10', 'TestUser', 'testtest@mail.ru', '$2y$12$6h7kGRjjIfzwZo4yBCZEJeeJSzf61fRhBai9Jbdzm1OSTP7Kpqt3C'); --123456
+     VALUES (true, '2018-04-05', 'Fyrklod', 'fyrklod@gmail.com', '$2a$12$SK3IzYargaC7bj9yAIGnV.OvLDSxIbOGPGZBaueaaMMyngtr4cu9K'),
+     (false, '2019-05-10', 'anderkaz', 'anderkaz@yandex.ru', '$2a$12$SK3IzYargaC7bj9yAIGnV.OvLDSxIbOGPGZBaueaaMMyngtr4cu9K'),
+     (false, '2020-06-10', 'JavaDeveloper', 'JavaDeveloper@yandex.ru', '$2a$12$SK3IzYargaC7bj9yAIGnV.OvLDSxIbOGPGZBaueaaMMyngtr4cu9K'),
+     (false, '2020-06-10', 'DotNetLover', 'DotNetLover@yandex.ru', '$2a$12$SK3IzYargaC7bj9yAIGnV.OvLDSxIbOGPGZBaueaaMMyngtr4cu9K'),
+     (false, '2020-06-10', 'ETLESB', 'etlesb@yandex.ru', '$2a$12$SK3IzYargaC7bj9yAIGnV.OvLDSxIbOGPGZBaueaaMMyngtr4cu9K'),
+     (false, '2020-06-10', 'SimpleUser', 'SimpleUser@yandex.ru', '$2a$12$SK3IzYargaC7bj9yAIGnV.OvLDSxIbOGPGZBaueaaMMyngtr4cu9K');
--- src/main/resources/db/migration/V5__Fiil_TableOfPostComment.sql
+++ src/main/resources/db/migration/V5__Fiil_TableOfPostComment.sql
@@ -1,3 +1,7 @@
 INSERT INTO post_comments
-    (post_id, user_id, "time", text)
-	VALUES (3, 1, '2019-06-02 05:30:24', 'testComment');
+    (post_id, parent_id, user_id, "time", text)
+	VALUES (1, null, 1, '2019-06-02 05:30:24', 'testComment'),
+	       (1, 1, 4, '2020-06-10 10:54:21', '<strong>Fyrklod</strong>, LOOOOL)))))'),
+           (1, 1, 3, '2020-06-10 11:30:14', '<strong>Fyrklod</strong>, WTF????'),
+           (1, 1, 5, '2020-06-10 11:30:14', '<strong>Fyrklod</strong>, Please clear comments for this post'),
+           (12, null, 1, '2020-12-16 05:30:24', 'Can you write example?');
